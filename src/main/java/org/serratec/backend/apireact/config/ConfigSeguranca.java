d4222a47000160564ddc41aec5e1e8d61a7b30ae
Gabriel Braga <bragagabriel843@gmail.com>
2024-11-05 19:49:28 -0300

    teste1

--- src/main/java/org/serratec/backend/apireact/config/ConfigSeguranca.java
+++ src/main/java/org/serratec/backend/apireact/config/ConfigSeguranca.java
@@ -36,9 +36,9 @@ public class ConfigSeguranca {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()).cors((cors) -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "https://apireact-214173757800.herokuapp.com/user/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "https://apireact-214173757800.herokuapp.com/user/email").permitAll()
-                        .requestMatchers(HttpMethod.POST, "https://apireact-214173757800.herokuapp.com/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/user/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/user/email").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
                         .anyRequest().authenticated()
 
                 ).httpBasic(Customizer.withDefaults())
