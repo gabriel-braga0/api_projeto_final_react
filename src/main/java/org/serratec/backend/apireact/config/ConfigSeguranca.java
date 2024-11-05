cc187a79c336a3f5cce0117b87ae1342b74c3408
Gabriel Braga <bragagabriel843@gmail.com>
2024-11-05 19:39:53 -0300

    adaptacao security deploy

--- src/main/java/org/serratec/backend/apireact/config/ConfigSeguranca.java
+++ src/main/java/org/serratec/backend/apireact/config/ConfigSeguranca.java
@@ -36,12 +36,10 @@ public class ConfigSeguranca {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()).cors((cors) -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**")
-                        .permitAll()
-                        .requestMatchers(HttpMethod.POST, "/user/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/user/email").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                        .anyRequest().permitAll()
+                        .requestMatchers(HttpMethod.POST, "https://apireact-214173757800.herokuapp.com/user/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "https://apireact-214173757800.herokuapp.com/user/email").permitAll()
+                        .requestMatchers(HttpMethod.POST, "https://apireact-214173757800.herokuapp.com/login").permitAll()
+                        .anyRequest().authenticated()
 
                 ).httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
